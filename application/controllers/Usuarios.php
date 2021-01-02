5a8d2ffbf639e709b5d6cd4b4e87d0b13789b7a4
Aderbal Cavalcante <aderc19@gmail.com>
2021-01-02 16:09:55 -0400

    Adicionadas validações com flashdata e configurando sessions

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -36,7 +36,9 @@ class Usuarios extends CI_Controller {
     public function edit($usuario_id = NULL) {
 
         if (!$usuario_id || !$this->ion_auth->user($usuario_id)->row()) {
-            exit('Usuário não encontrado!');
+            
+            $this->session->set_flashdata('error','Usuário não encontrado');
+            redirect('usuarios');
         } else {
 
 //            [first_name] => Admin
@@ -53,6 +55,11 @@ class Usuarios extends CI_Controller {
 //            exit();
 
             $this->form_validation->set_rules('first_name', '', 'trim|required');
+            $this->form_validation->set_rules('last_name', '', 'trim|required');
+            $this->form_validation->set_rules('email', '', 'trim|required');
+            $this->form_validation->set_rules('username', '', 'trim|required');
+            $this->form_validation->set_rules('password', '', 'trim|required');
+            $this->form_validation->set_rules('confirm_password', '', 'trim|required');
 
             if ($this->form_validation->run()) {
                 exit('Validado');
